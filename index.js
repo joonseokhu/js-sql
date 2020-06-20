8ed3025ebf66b1272dc8dfc3f4a71cd1b65a1364
허준석 <hujoonseok@Hu-Joonseok-MacBookPro.local>
2020-06-21 03:41:15 +0900

    init

---
+++ index.js
@@ -0,0 +1,105 @@
+const mysql = require('mysql2/promise')
+
+const pipe = (...fns) => (
+  (value) => (
+    fns.reduce((prev, fn) => fn(prev), value)
+  )
+)
+
+const setKey = (root, keys, value) => {
+  let node = root
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i]
+    const isLeaf = (i === (keys.length - 1))
+    const hasKey = Object.prototype.hasOwnProperty.call(node, key)
+    if (isLeaf) {
+      node[key] = value
+      break
+    }
+    if (!hasKey) {
+      node[key] = {}
+    }
+    node = node[key]
+  }
+}
+
+const sanitizeStr = pipe(
+  e => e.replace(/\\/g, '\\\\'),
+  e => e.replace(/'/g, '\\\''),
+  e => e.replace(/"/g, '\\\"'),
+  e => `'${e}'`,
+)
+
+const sanitizeObj = pipe(
+  e => JSON.stringify(e),
+  sanitizeStr,
+)
+
+const sanitizeValue = (value) => {
+  switch (typeof value) {
+    case 'string': return sanitizeStr(value);
+    case 'boolean': return value ? 'true' : 'false';
+    case 'number': return value.toString();
+    case 'object': return (value === null) ? 'null' : sanitizeObj(value);
+    default: return ''
+  }
+}
+
+const insert = (table, obj) => {
+  const keys = [];
+  const values = [];
+  const pairs = Object.entries(obj)
+  for (const pair of Object.entries(obj)) {
+    const [key, value] = pair
+    keys.push(key)
+    values.push(sanitizeValue(value))
+  }
+  return `insert into ${table} (
+    ${keys.join(',\n    ')}
+) values (
+    ${values.join(',\n    ')}
+)`
+}
+
+const select = (obj) => {
+  const ret = []
+  const formatSelects = (obj, root = []) => Object.entries(obj).map(([key, value]) => {
+    const keys = [...root, key]
+    if (typeof value === 'string') {
+      return ret.push(`${value} as '${keys.join('.')}'`)
+    }
+    if (typeof value === 'object') return formatSelects(value, keys)
+  })
+
+  formatSelects(obj)
+
+  console.log(ret)
+  // const selects = Object.entries(obj).map(([key, value]) => {
+  //   if (typeof value === 'string') return `${value} as '${key}'`
+  //   if (typeof value === 'object') {
+  //     Object.entries(value)
+  //     return `${value} as '${key}'`
+  //   }
+  //   return `${value} as '${key}'`
+  // })
+  
+  return `select
+    ${ret.join(',\n    ')}
+`
+}
+
+const formatResult = (rows) => rows.map((row) => {
+  const ret = {}
+  for(const el of Object.entries(row)) {
+    const [key, value] = el
+    const keys = key.split('.')
+    setKey(ret, keys, value)
+  }
+  return ret
+})
+
+module.exports = {
+  insert,
+  select,
+  formatResult,
+}
---
+++ test.js
@@ -0,0 +1,73 @@
+const mysql = require('mysql2/promise')
+
+const { insert, select, formatResult } = require('./index')
+
+const test = (str) => {
+  console.log([...Array(30).keys()].map(e => '\n').join(''))
+  console.log(str)
+  return connection.query(str)
+  .then(([res]) => {
+    console.log('success')
+    const result = formatResult(res)
+    console.log(result)
+    return result
+  })
+  .catch(err => {
+    console.log('error')
+    console.log(err)
+    return []
+  })
+}
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'testdb',
+  database: 'testdb',
+  password: 'TGkCpGQ37$HZxe',
+})
+
+// const foo = 'test5'
+// test(insert('user', {
+//   username: foo,
+//   password: '1234',
+//   email: `${foo}@test.com`,
+//   name: null
+// }))
+
+// test(insert('article', {
+//   user_id: 40,
+//   title: '제목8',
+//   content: '내용내용내용',
+// }))
+
+// test(`${select({
+//   'user.id': 'index',
+//   'user.username': 'id',
+//   'user.created_at': 'createdAt'
+// })}
+// from user
+// `).then(users => users.map(user => {
+//   try {
+//     console.log('~')
+//     console.log(user.index)
+//     console.log(user.id)
+//     console.log(JSON.parse(user.id))
+//   } catch (e) {
+//     console.log(user.id)
+//   }
+//   return ''
+// }))
+
+test(`${select({
+  id: 'article.id',
+  title: 'article.title',
+  content: 'article.content',
+  article_user_id: 'article.user_id',
+  user: {
+    id: 'user.id',
+    username: 'user.username',
+  },
+})}
+from article
+left join user on article.user_id = user.id
+`)
